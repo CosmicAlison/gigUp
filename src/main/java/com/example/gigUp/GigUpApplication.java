4c56d48c350228fff27fcadf6eec66d93985e0d6
Alison Pariela <parielaalison@gmail.com>
2024-11-18 13:03:43 +0400

    adding boilerplate files

---
+++ src/main/java/com/example/gigUp/GigUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.gigUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gigUp/GigUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gigUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
